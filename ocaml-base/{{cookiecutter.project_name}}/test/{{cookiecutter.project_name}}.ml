5d60c28afec9e981a3acee32e7ef78c84a286821
Jochen Bartl <jochenbartl@mailbox.org>
2022-07-27 21:13:26 +0200

    add alcotest to ocaml

--- ocaml-base/{{cookiecutter.project_name}}/test/{{cookiecutter.project_name}}.ml
+++ ocaml-base/{{cookiecutter.project_name}}/test/{{cookiecutter.project_name}}.ml
@@ -1,1 +1,13 @@
-let () = print_endline "test are mkay"
+let test_one_is_not_two () =
+  Alcotest.(check bool) "one is not two" (1 = 2) false
+
+let () =
+  let open Alcotest in
+  run "{{cookiecutter.project_name}}"
+    [
+      ("basic-tests",
+        [
+          test_case "one is not two" `Quick test_one_is_not_two;
+        ]
+      );
+    ]
